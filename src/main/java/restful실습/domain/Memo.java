457f643b5b7b070ce00a345cf70682a460adef83
jihyun <jihyun3066@naver.com>
2025-02-24 11:34:54 +0900

    [ feat ] responseEntity 실습

---
+++ src/main/java/restful실습/domain/Memo.java
@@ -0,0 +1,13 @@
+package restful실습.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Getter
+@Setter
+public class Memo {
+    private Long id;
+    private String content;
+}
